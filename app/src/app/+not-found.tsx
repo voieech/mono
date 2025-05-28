c0a26f75a24339991fac98b5130b7e7eac0dc630
Jaime <jaimeloeuf@gmail.com>
2025-05-29 00:18:36 +0800

    Update app not found page.

--- app/src/app/+not-found.tsx
+++ app/src/app/+not-found.tsx
@@ -1,3 +1,4 @@
+import { Image } from "expo-image";
 import { Link, Stack } from "expo-router";
 import { StyleSheet } from "react-native";
 
@@ -7,11 +8,16 @@ import { ThemedView } from "@/components/ThemedView";
 export default function NotFoundScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: "Oops!" }} />
+      <Stack.Screen options={{ title: "OOPS!" }} />
       <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen does not exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
+        <ThemedText type="subtitle">Oops sorry this does not exist!</ThemedText>
+        <Image
+          source={require("@/assets/images/404.svg")}
+          style={styles.notFoundImage}
+          alt="Not Found Image"
+        />
+        <Link href="/" style={styles.link} replace>
+          <ThemedText type="link">Go back home</ThemedText>
         </Link>
       </ThemedView>
     </>
@@ -24,6 +30,10 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     padding: 20,
+  },
+  notFoundImage: {
+    height: "50%",
+    resizeMode: "contain",
   },
   link: {
     marginTop: 15,
