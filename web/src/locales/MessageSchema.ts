763648dc91d2644f1bb282d0d3c0d701742cd976
Jaime <jaimeloeuf@gmail.com>
2025-06-12 21:05:22 +0800

    Add support for $t generic type based hints.

--- web/src/locales/MessageSchema.ts
+++ web/src/locales/MessageSchema.ts
@@ -3,3 +3,10 @@ import type en from "./en";
 
 // Type-define 'en' as the master schema for the resource
 export type MessageSchema = DeepPartial<typeof en>;
+
+type FullMessageSchema = typeof en;
+
+declare module "vue-i18n" {
+  // Extend this to get generic TS based hints for the `$t` function args.
+  interface DefineLocaleMessage extends FullMessageSchema {}
+}
