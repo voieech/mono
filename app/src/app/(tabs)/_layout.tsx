813e14049c51474ea6206261047c996436e790f3
Jaime <jaimeloeuf@gmail.com>
2025-06-16 00:45:35 +0800

    wip refactor.

--- app/src/app/(tabs)/_layout.tsx
+++ app/src/app/(tabs)/_layout.tsx
@@ -1,7 +1,5 @@
 import { Tabs } from "expo-router";
-import React from "react";
 import { Platform } from "react-native";
-
 import { IconSymbol } from "@/components/ui/IconSymbol";
 import TabBarBackground from "@/components/ui/TabBarBackground";
 import { useTheme } from "@/hooks/useTheme";
--- app/src/app/_layout.tsx
+++ app/src/app/_layout.tsx
@@ -6,10 +6,8 @@ import {
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import "react-native-reanimated";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-import { useColorScheme } from "react-native";
+import { useTheme } from "@/hooks/useTheme";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -30,21 +28,26 @@ const queryClient = new QueryClient({
 });
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
+  const theme = useTheme();
+  const [isFontLoaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
-  if (!loaded) {
-    // Async font loading only occurs in development.
+  // Wait for font to load. This should not run in production as async font
+  // loading only occurs in development.
+  if (!isFontLoaded) {
     return null;
   }
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={theme === "dark" ? DarkTheme : DefaultTheme}>
       <QueryClientProvider client={queryClient}>
         <StatusBar style="auto" />
-        <Stack>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
           <Stack.Screen
             name="(tabs)"
             options={{
