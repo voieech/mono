9a74f35381e188bf9b2cfbaf7ff2317cc8f8293d
Jaime <jaimeloeuf@gmail.com>
2025-06-20 17:55:20 +0800

    Refactor podcast episode page to use global player.

--- app/src/app/(tabs)/(home)/podcast/episode/[vanityID].tsx
+++ app/src/app/(tabs)/(home)/podcast/episode/[vanityID].tsx
@@ -1,16 +1,24 @@
+import type { Episode } from "dto";
+
 import { useQuery } from "@tanstack/react-query";
-import { Episode } from "dto";
 import { Image } from "expo-image";
 import { useRouter, useLocalSearchParams } from "expo-router";
+import { useCallback } from "react";
+import { Pressable, View } from "react-native";
+import TrackPlayer, {
+  State as PlayerState,
+  useActiveTrack,
+  usePlaybackState,
+} from "react-native-track-player";
 
 import {
+  IconSymbol,
   ParallaxScrollViewContainer,
   FullScreenLoader,
   ThemedView,
   ThemedText,
-  AudioPlayer,
 } from "@/components";
-import { apiBaseUrl } from "@/constants";
+import { apiBaseUrl, Colors } from "@/constants";
 
 export default function PodcastEpisode() {
   const router = useRouter();
@@ -42,6 +50,47 @@ export default function PodcastEpisode() {
       return (await res.json()) as Episode;
     },
   });
+
+  const activeTrack = useActiveTrack();
+
+  // Making the assumption that titles are unique
+  const isCurrentEpisodeTheActiveTrack =
+    activeTrack !== undefined &&
+    episode !== undefined &&
+    activeTrack.title === episode.title;
+
+  const playEpisode = useCallback(
+    async function () {
+      // Should not happen since UI wont be shown until episode is loaded
+      if (episode === undefined) {
+        return;
+      }
+
+      // If current episode is the active track, just continue playing
+      if (isCurrentEpisodeTheActiveTrack) {
+        await TrackPlayer.play();
+        return;
+      }
+
+      // If there is a track queue already, replace the current track with this,
+      // else, create a new queue and add this as the new current active track.
+      await TrackPlayer.load({
+        // Artist is always voieech for pre-generated podcasts, will be
+        // different if it is user generated content in the future.
+        artist: "voieech.com",
+        url: episode.audio_public_url,
+        title: episode.title,
+        duration: episode.audio_length,
+        // @todo Remove backup once img_url is not nullable
+        artwork: episode.img_url ?? require("@/assets/images/logo.png"),
+      });
+
+      await TrackPlayer.play();
+    },
+    [episode, isCurrentEpisodeTheActiveTrack]
+  );
+
+  const playerState = usePlaybackState().state;
 
   if (isPending) {
     return <FullScreenLoader />;
@@ -82,36 +131,66 @@ export default function PodcastEpisode() {
         )
       }
     >
-      <ThemedView>
-        <ThemedView
+      <ThemedText type="subtitle">{episode.title}</ThemedText>
+      {/* @todo Add link back to the channel page */}
+      <ThemedView
+        style={{
+          paddingVertical: 20,
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignContent: "center",
+        }}
+      >
+        <ThemedText
           style={{
-            gap: 8,
-            marginBottom: 8,
+            fontSize: 14,
+            lineHeight: 20,
           }}
         >
-          <ThemedText type="subtitle">{episode.title}</ThemedText>
-          <ThemedText>{episode.description}</ThemedText>
-        </ThemedView>
-
-        <ThemedView
+          Season {episode.season_number}, Episode {episode.episode_number}
+          {"\n"}
+          {episode.created_at.split("T")[0]}
+          {"\n"}
+          {Math.trunc(episode.audio_length / 60)} mins
+        </ThemedText>
+        <View
           style={{
-            gap: 8,
-            marginBottom: 8,
+            justifyContent: "center",
           }}
         >
-          <AudioPlayer
-            url={episode.audio_public_url}
-            title={
-              episode.episode_number
-                ? `EP ${episode.episode_number}: ${episode.title}`
-                : episode.title
-            }
-            audioLength={episode.audio_length}
-            // @todo Only set this if it is a custom made track by someone else
-            // artist={}
-          />
-        </ThemedView>
+          <View
+            style={{
+              padding: 8,
+              backgroundColor: Colors.dark.text,
+              borderRadius: "50%",
+            }}
+          >
+            {isCurrentEpisodeTheActiveTrack &&
+            playerState === PlayerState.Playing ? (
+              <Pressable onPress={TrackPlayer.pause}>
+                <IconSymbol name="pause.fill" color="black" />
+              </Pressable>
+            ) : (
+              <Pressable onPress={playEpisode}>
+                <IconSymbol name="play.fill" color="black" />
+              </Pressable>
+            )}
+          </View>
+        </View>
       </ThemedView>
+      <View
+        style={{
+          borderTopColor: "#777",
+          borderTopWidth: 0.5,
+          paddingBottom: 20,
+        }}
+      />
+      <ThemedText>{episode.description}</ThemedText>
+      <ThemedView
+        style={{
+          paddingBottom: 64,
+        }}
+      />
     </ParallaxScrollViewContainer>
   );
 }
