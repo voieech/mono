a13d8db5e5dce10a6f70d6c6aded6cb6b4506471
Jaime <jaimeloeuf@gmail.com>
2025-06-28 00:56:15 +0800

    Add docs for setting context fields.

--- app/src/context/settingsContext.ts
+++ app/src/context/settingsContext.ts
@@ -56,8 +56,21 @@ export type SettingState = {
 };
 
 export const SettingContext = createContext<{
+  /**
+   * "Re-exporting" settings object so that users of this context can access the
+   * main settings object from the context object without having to do an
+   * additional import of the settings object
+   */
   settings: typeof settings;
+
+  /**
+   * Get setting with the correct generic type returned
+   */
   getSetting: <T extends SettingNames>(setting: T) => SettingState[T];
+
+  /**
+   * Update setting based on the allowed value type
+   */
   updateSetting: <T extends SettingNames>(
     setting: T,
     value: SettingState[T]
