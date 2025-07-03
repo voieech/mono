503699162f60d117fbee505ba955bebfeda21719
Jaime <jaimeloeuf@gmail.com>
2025-07-04 00:28:03 +0800

    Fix JSDoc placement.

--- app/src/hooks/useTheme.ts
+++ app/src/hooks/useTheme.ts
@@ -1,12 +1,11 @@
-/**
- * Wrapper over native `useColorScheme` to always give a value back, defaulting
- * to light theme if unable to load a color scheme.
- */
-
 import { useColorScheme } from "react-native";
 
 import { isThemeFixed, DefaultTheme } from "@/constants/FixedTheme";
 
+/**
+ * Wrapper over native `useColorScheme` to always give a value back, defaulting
+ * to light theme if unable to load a color scheme.
+ */
 export function useTheme() {
   const colorScheme = useColorScheme() ?? DefaultTheme;
 
