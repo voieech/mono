a7837114ee0bc2145b7f544c8485aa824cd2683b
Jaime <jaimeloeuf@gmail.com>
2025-07-05 20:34:11 +0800

    Capture setting updates with posthog.

--- app/src/components/Settings.tsx
+++ app/src/components/Settings.tsx
@@ -1,7 +1,12 @@
 import { useState, type PropsWithChildren } from "react";
 
 import { SettingContext } from "@/context";
-import { settings, defaultSettingState, type SettingState } from "@/utils";
+import {
+  settings,
+  defaultSettingState,
+  type SettingState,
+  posthog,
+} from "@/utils";
 
 export function SettingsProvider(props: PropsWithChildren) {
   const [settingState, setSettingState] =
@@ -14,6 +19,8 @@ export function SettingsProvider(props: PropsWithChildren) {
           return settingState[setting];
         },
         updateSetting(setting, newValue) {
+          const oldValue = settingState[setting] as any;
+
           setSettingState((state) => ({
             ...state,
             [setting]: newValue,
@@ -22,10 +29,13 @@ export function SettingsProvider(props: PropsWithChildren) {
           // Call the onChange callback, the types are widened to be `any` here
           // since the types can be generic on the extended Setting interfaces
           // e.g. a string literal type is used
-          settings[setting]?.onChange?.(
-            newValue as any,
-            settingState[setting] as any,
-          );
+          settings[setting]?.onChange?.(newValue as any, oldValue);
+
+          posthog.capture("setting_update", {
+            setting,
+            oldValue,
+            newValue,
+          });
         },
       }}
     >
