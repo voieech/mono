f828b09ce64f8ceab2b905a2bc3313d631c79443
Jaime <jaimeloeuf@gmail.com>
2025-07-09 00:11:37 +0800

    Add arrow to indicate voieech app button is a link.

--- web/src/views/PodcastChannel/PodcastChannel.vue
+++ web/src/views/PodcastChannel/PodcastChannel.vue
@@ -91,6 +91,21 @@ useHead({
             />
             <div class="flex w-full flex-row items-center justify-between">
               <p>Voieech App</p>
+              <svg
+                class="h-6 w-6 shrink-0 rotate-90 text-yellow-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 10 6"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M9 5 5 1 1 5"
+                />
+              </svg>
             </div>
           </div>
         </a>
