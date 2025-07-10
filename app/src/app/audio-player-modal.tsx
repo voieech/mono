0c95b2ad66576e6aad188370e960291aa78dee4b
Jaime <jaimeloeuf@gmail.com>
2025-07-11 01:01:37 +0800

    Add tmp modal to "more" menu in global player.

--- app/src/app/audio-player-modal.tsx
+++ app/src/app/audio-player-modal.tsx
@@ -1,5 +1,12 @@
 import { router } from "expo-router";
-import { Pressable, useWindowDimensions } from "react-native";
+import React, { useState } from "react";
+import {
+  Pressable,
+  useWindowDimensions,
+  Modal,
+  Text,
+  View,
+} from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import {
@@ -14,6 +21,7 @@ import { useActiveTrackWithMetadata } from "@/hooks";
 export default function AudioPlayerModal() {
   const windowDimensions = useWindowDimensions();
   const activeTrack = useActiveTrackWithMetadata();
+  const [modalVisible, setModalVisible] = useState(false);
   return (
     <SafeScrollViewContainer>
       {/*
@@ -47,16 +55,74 @@ export default function AudioPlayerModal() {
           >
             {activeTrack?.artist}
           </ThemedText>
-          <Pressable
-            onPress={() => {
-              //
-            }}
-          >
+          <Pressable onPress={() => setModalVisible(true)}>
             <Icon name="ellipsis" color="white" />
           </Pressable>
         </ThemedView>
         <AudioPlayer />
       </GestureHandlerRootView>
+      <Modal
+        visible={modalVisible}
+        animationType="slide"
+        transparent={true}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <View
+            style={{
+              margin: 20,
+              backgroundColor: "white",
+              borderRadius: 20,
+              padding: 35,
+              alignItems: "center",
+              shadowColor: "#000",
+              shadowOffset: {
+                width: 0,
+                height: 2,
+              },
+              shadowOpacity: 0.25,
+              shadowRadius: 4,
+              elevation: 5,
+            }}
+          >
+            <Text
+              style={{
+                marginBottom: 15,
+                textAlign: "center",
+              }}
+            >
+              Made by {activeTrack?.artist}
+            </Text>
+            <Pressable
+              style={{
+                borderRadius: 20,
+                padding: 10,
+                elevation: 2,
+                backgroundColor: "#2196F3",
+              }}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text
+                style={{
+                  color: "white",
+                  fontWeight: "bold",
+                  textAlign: "center",
+                }}
+              >
+                close
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
     </SafeScrollViewContainer>
   );
 }
