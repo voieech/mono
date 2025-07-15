7893f1ec921088fbc47e2e5afb32856f2625a12c
Jaime <jaimeloeuf@gmail.com>
2025-07-15 19:47:35 +0800

    Refactor out settings localStorage wrapper.

--- app/src/utils/settings/Settings.ts
+++ app/src/utils/settings/Settings.ts
@@ -1,6 +1,5 @@
 import type { SettingUnion } from "./types/SettingUnion";
 
-import { localStorage } from "../localStorage";
 import { defaultPlaybackSpeed } from "./defaultPlaybackSpeed";
 import { externalMediaControls } from "./externalMediaControls";
 
@@ -43,43 +42,3 @@ export const defaultSettingState = Object.entries(settings).reduce(
   },
   {} as Partial<SettingState>,
 ) as SettingState;
-
-/**
- * Wrapper around `localStorage` API for settings
- */
-export const settingsInLocalStorage = {
-  settingsStorageKey: "settings",
-
-  /**
-   * Returns settings data in localStorage. This will run a side effect of
-   * writing default settings data to localStorage if no data is read the first
-   * time.
-   */
-  async read() {
-    const [err, data] = await localStorage.readData<SettingState>(
-      this.settingsStorageKey,
-    );
-
-    if (err !== null) {
-      // Only on first use, since settings data not in local storage yet, we
-      // will write the default settings in
-      if (err.name === localStorage.notFoundErrorName) {
-        await this.resetToDefault();
-        return defaultSettingState;
-      }
-
-      // If it is some other unknown error, re-throw it!
-      throw err;
-    }
-
-    return data;
-  },
-
-  update(settings: SettingState) {
-    return localStorage.writeData(this.settingsStorageKey, settings);
-  },
-
-  resetToDefault() {
-    return this.update(defaultSettingState);
-  },
-} as const;
