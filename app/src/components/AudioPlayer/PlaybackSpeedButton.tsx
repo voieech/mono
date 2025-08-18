fd79df8cc50d4276985e81535c1221c7c4597295
Marshmallos <oknainot@gmail.com>
2025-08-18 21:33:32 +0800

    feat: add function to update playbackspeed

--- app/src/components/AudioPlayer/PlaybackSpeedButton.tsx
+++ app/src/components/AudioPlayer/PlaybackSpeedButton.tsx
@@ -1,22 +1,45 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Pressable } from "react-native";
 
 import { ThemedText } from "@/components";
 
-export function PlaybackSpeedButton() {
-  const [speedIndex, setSpeedIndex] = useState(0);
+export function PlaybackSpeedButton(props: {
+  getTrackPlaybackRate: () => Promise<number>;
+  setTrackPlaybackRate: (rate: number) => Promise<void>;
+}) {
   const allPlaybackSpeed = [0.75, 1, 1.5, 2];
+  const [trackPlaybackSpeed, setTrackPlaybackSpeed] = useState(1);
+  const [isLoading, setIsLoading] = useState(true);
 
-  function getPlaybackSpeed() {
-    setSpeedIndex((prev) => (prev + 1) % allPlaybackSpeed.length);
+  useEffect(() => {
+    const fetchData = async () => {
+      const trackPlaybackRate = await props.getTrackPlaybackRate();
+      setTrackPlaybackSpeed(trackPlaybackRate);
+      setIsLoading(false);
+    };
+    fetchData();
+  });
+
+  function handleOnClick() {
+    const currentIndex = allPlaybackSpeed.indexOf(trackPlaybackSpeed);
+    const newSpeed =
+      allPlaybackSpeed[(currentIndex + 1) % allPlaybackSpeed.length];
+
+    if (newSpeed !== undefined) {
+      props.setTrackPlaybackRate(newSpeed);
+      setTrackPlaybackSpeed(newSpeed);
+    }
+  }
+
+  if (isLoading) {
+    return <ThemedText style={{ paddingTop: 10 }}>Loading...</ThemedText>;
   }
 
   return (
-    <Pressable onPress={getPlaybackSpeed}>
+    <Pressable onPress={handleOnClick}>
       <ThemedText style={{ fontSize: 28, paddingTop: 10 }}>
-        {allPlaybackSpeed[speedIndex]}x
+        {trackPlaybackSpeed}x
       </ThemedText>
-      ;
     </Pressable>
   );
 }
