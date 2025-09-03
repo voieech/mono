c8ddaeabcb9ce2cfd492bc888ce477999adc66d2
Marshmallos <oknainot@gmail.com>
2025-09-03 16:57:36 +0800

    fix: embed posthog api key as build cannot read

--- app/src/utils/posthog.ts
+++ app/src/utils/posthog.ts
@@ -1,7 +1,9 @@
 import PostHog from "posthog-react-native";
-import Config from "react-native-config";
 
-export const posthog = new PostHog(Config.POSTHOG_API_KEY, {
-  disabled: __DEV__,
-  captureAppLifecycleEvents: true,
-});
+export const posthog = new PostHog(
+  "phc_2c404JOxNanNDgJNdagtfwqc95BNVTIJVQLsMB75uh5",
+  {
+    disabled: __DEV__,
+    captureAppLifecycleEvents: true,
+  },
+);
