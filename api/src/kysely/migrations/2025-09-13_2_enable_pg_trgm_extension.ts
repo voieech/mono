8e8c4ea662659135bda07494ecffff1678e0ef57
Jaime <jaimeloeuf@gmail.com>
2025-09-13 20:40:46 +0800

    Add migration to enable pg_trgm extension.

---
+++ api/src/kysely/migrations/2025-09-13_2_enable_pg_trgm_extension.ts
@@ -0,0 +1,11 @@
+import type { Kysely } from "kysely";
+
+import { sql } from "kysely";
+
+export async function up(db: Kysely<any>): Promise<void> {
+  await sql`CREATE EXTENSION IF NOT EXISTS pg_trgm;`.execute(db);
+}
+
+export async function down(db: Kysely<any>): Promise<void> {
+  await sql`DROP EXTENSION IF EXISTS pg_trgm;`.execute(db);
+}
