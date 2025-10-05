4ae24cb399ee1c53697e8d51e72d498e4593ddf9
Jaime <jaimeloeuf@gmail.com>
2025-10-05 23:03:07 +0800

    Add basic reccomendations API.

--- api/src/http/bootstrapHttpServer.ts
+++ api/src/http/bootstrapHttpServer.ts
@@ -78,6 +78,62 @@ export function bootstrapHttpServer() {
       res.status(200).json(featuredEpisodes satisfies Array<Episode>);
     })
 
+    /**
+     * Get a list of reccomendations to play next after a given podcast
+     * episode's vanity ID.
+     *
+     * @todo things to try
+     * 1. vector DB to store embedding of all the stuff user listened + liked
+     * then show them relevant stuff?
+     */
+    .get("/v1/podcast/reccomendations/next", async function (req, res) {
+      const rawLimit = Number(req.query["limit"]);
+      const limit = isNaN(rawLimit) || rawLimit === 0 ? 5 : rawLimit;
+      if (limit < 1 || limit > 20) {
+        res.status(400).json({
+          error: "'limit' must be between 1 and 20",
+        });
+        return;
+      }
+
+      const currentEpisodeVanityID = req.query["current_episode_vanity_id"];
+      if (
+        typeof currentEpisodeVanityID !== "string" ||
+        currentEpisodeVanityID === undefined
+      ) {
+        res.status(400).json({
+          error: "'current_episode_vanity_id' is invalid",
+        });
+        return;
+      }
+
+      const currentEpisode = await apiDB
+        .selectFrom("podcast_episode")
+        .selectAll("podcast_episode")
+        .where("podcast_episode.vanity_id", "=", currentEpisodeVanityID)
+        .executeTakeFirst();
+      if (currentEpisode === undefined) {
+        res.status(404).json({
+          error: `Cannot find episode with VanityID: ${currentEpisodeVanityID}`,
+        });
+        return;
+      }
+
+      // @todo Filter out all episodes that the user listened to before
+      // Get latest episodes from same channel as reccomendations for now
+      const reccomendations = await genPodcastEpisodeBaseQuery()
+        .limit(limit)
+        .where("podcast_episode.channel_id", "=", currentEpisode.channel_id)
+        // Filter out the current episode user is listening to
+        .where("podcast_episode.id", "!=", currentEpisode.id)
+        .orderBy("podcast_episode.created_at", "desc")
+        .execute();
+
+      res.status(200).json({
+        reccomendations: reccomendations satisfies Array<Episode>,
+      });
+    })
+
     .get("/v1/podcast/episode/:vanityID", async function (req, res) {
       const vanityID = req.params.vanityID;
 
