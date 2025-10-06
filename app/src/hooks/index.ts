872608b797026d1fbe09820d20c842074660b4ed
Jaime <jaimeloeuf@gmail.com>
2025-10-06 23:12:09 +0800

    Create utility hook to return player current queue.

--- app/src/hooks/index.ts
+++ app/src/hooks/index.ts
@@ -6,5 +6,6 @@ export * from "./api/index";
 export * from "./useActiveTrackWithMetadata";
 export * from "./useBottomTabOverflow";
 export * from "./useColorScheme.web";
+export * from "./usePlayerQueue";
 export * from "./useTheme";
 export * from "./useThemeColor";
