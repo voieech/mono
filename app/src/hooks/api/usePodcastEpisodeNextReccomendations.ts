40d33110e4b53239ec3eebdac73b0e2fc56856f4
Jaime <jaimeloeuf@gmail.com>
2025-10-06 23:03:23 +0800

    Allow user to specify reccomendations limit.

--- app/src/hooks/api/usePodcastEpisodeNextReccomendations.ts
+++ app/src/hooks/api/usePodcastEpisodeNextReccomendations.ts
@@ -7,19 +7,25 @@ import { apiBaseUrl } from "@/constants";
 /**
  * Useful for showing reccomendations related to the current episode
  */
-export function usePodcastEpisodeNextReccomendations(vanityID: string) {
+export function usePodcastEpisodeNextReccomendations(
+  vanityID: string,
+  limit?: number,
+) {
   return useQuery({
     queryKey: ["podcast", "episode", "reccomendations", vanityID],
-    queryFn: () => getPodcastEpisodeNextReccomendations(vanityID),
+    queryFn: () => getPodcastEpisodeNextReccomendations(vanityID, limit),
   });
 }
 
 /**
  * Get a list of podcast episodes that are reccomended for given current episode
  */
-export async function getPodcastEpisodeNextReccomendations(vanityID: string) {
+export async function getPodcastEpisodeNextReccomendations(
+  vanityID: string,
+  limit?: number,
+) {
   const res = await fetch(
-    `${apiBaseUrl}/v1/podcast/reccomendations/next?current_episode_vanity_id=${vanityID}&limit=10`,
+    `${apiBaseUrl}/v1/podcast/reccomendations/next?current_episode_vanity_id=${vanityID}&limit=${limit ?? 10}`,
   );
 
   if (!res.ok) {
