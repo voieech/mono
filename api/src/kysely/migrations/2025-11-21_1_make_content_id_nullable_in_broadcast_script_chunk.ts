3cfc461b6d2ebdb55277c6c65cfef814be682932
Jaime <jaimeloeuf@gmail.com>
2025-11-21 23:50:52 +0800

    Make broadcast_script_chunk table content_id nullable.

---
+++ api/src/kysely/migrations/2025-11-21_1_make_content_id_nullable_in_broadcast_script_chunk.ts
@@ -0,0 +1,21 @@
+import type { Kysely } from "kysely";
+
+const broadcastScriptChunkTableName = "broadcast_script_chunk";
+
+export async function up(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .alterTable(broadcastScriptChunkTableName)
+    .alterColumn("content_id", (alterColumn) => alterColumn.dropNotNull())
+    .execute();
+}
+
+// ********************************* DANGEROUS *********************************
+// THIS IS DANGEROUS because there might already be null columns so you cant do
+// this until you have filled every single nullable column first, or drop every
+// row with a null content_id.
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .alterTable(broadcastScriptChunkTableName)
+    .alterColumn("content_id", (alterColumn) => alterColumn.setNotNull())
+    .execute();
+}
