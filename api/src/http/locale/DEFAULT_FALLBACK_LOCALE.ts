9a445a73504dfd9f7fc275c7e3c11b6943d35935
Jaime <jaimeloeuf@gmail.com>
2025-12-02 19:53:48 +0800

    Add array for default fallback locales.

--- api/src/http/locale/DEFAULT_FALLBACK_LOCALE.ts
+++ api/src/http/locale/DEFAULT_FALLBACK_LOCALE.ts
@@ -2,3 +2,8 @@
  * Default locale used for the app if no locale is explicitly specified.
  */
 export const DEFAULT_FALLBACK_LOCALE = "en";
+
+/**
+ * Default locales used for the app if no locales is explicitly specified.
+ */
+export const DEFAULT_FALLBACK_LOCALES = [DEFAULT_FALLBACK_LOCALE];
