dda2453614ae2f33e9f5597125730de24c6a5c72
Jaime <jaimeloeuf@gmail.com>
2025-12-19 00:07:03 +0800

    Add in OldThemedLink.

--- app/src/components/ThemedText.tsx
+++ app/src/components/ThemedText.tsx
@@ -93,3 +93,24 @@ export function OldThemedText({
       throw new Error("Invalid text type", type);
   }
 }
+
+export function OldThemedLink({
+  style,
+  lightColor,
+  darkColor,
+  type,
+  ...rest
+}: TextProps & {
+  lightColor?: string;
+  darkColor?: string;
+  type?: string;
+}) {
+  const color = useThemeColor("text", { light: lightColor, dark: darkColor });
+  return (
+    <ThemedText
+      type={type as any}
+      style={[{ color }, TextTypeStyles.link, style]}
+      {...rest}
+    />
+  );
+}
