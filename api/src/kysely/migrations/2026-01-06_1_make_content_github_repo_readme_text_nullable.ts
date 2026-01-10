1a0f470206cc92dce56c8371531f6306cb892236
Jaime <jaimeloeuf@gmail.com>
2026-01-10 18:34:50 +0800

    Add DB update to prevent down migration breakage.

--- api/src/kysely/migrations/2026-01-06_1_make_content_github_repo_readme_text_nullable.ts
+++ api/src/kysely/migrations/2026-01-06_1_make_content_github_repo_readme_text_nullable.ts
@@ -14,6 +14,14 @@ export async function up(db: Kysely<any>): Promise<void> {
 // this until you have filled every single nullable column first, or drop every
 // row with a null readme_text.
 export async function down(db: Kysely<any>): Promise<void> {
+  await db
+    .updateTable(contentGithubRepoTableName)
+    .set({
+      readme_text: "__NULL__",
+    })
+    .where("readme_text", "is", null)
+    .execute();
+
   await db.schema
     .alterTable(contentGithubRepoTableName)
     .alterColumn("readme_text", (alterColumn) => alterColumn.setNotNull())
