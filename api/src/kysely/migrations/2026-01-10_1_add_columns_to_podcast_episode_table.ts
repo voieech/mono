dad55a89b68be1af7a5412aa03d96caf469a7908
Jaime <jaimeloeuf@gmail.com>
2026-01-10 17:10:16 +0800

    Make new columns default to null.

--- api/src/kysely/migrations/2026-01-10_1_add_columns_to_podcast_episode_table.ts
+++ api/src/kysely/migrations/2026-01-10_1_add_columns_to_podcast_episode_table.ts
@@ -10,10 +10,20 @@ const youtube_video_count_comment_column_name = "youtube_video_count_comment";
 export async function up(db: Kysely<any>): Promise<void> {
   await db.schema
     .alterTable(podcastEpisodeTableName)
-    .addColumn(youtube_video_count_last_updated_date_column_name, "timestamp")
-    .addColumn(youtube_video_count_view_column_name, "integer")
-    .addColumn(youtube_video_count_like_column_name, "integer")
-    .addColumn(youtube_video_count_comment_column_name, "integer")
+    .addColumn(
+      youtube_video_count_last_updated_date_column_name,
+      "timestamp",
+      (col) => col.defaultTo(null),
+    )
+    .addColumn(youtube_video_count_view_column_name, "integer", (col) =>
+      col.defaultTo(null),
+    )
+    .addColumn(youtube_video_count_like_column_name, "integer", (col) =>
+      col.defaultTo(null),
+    )
+    .addColumn(youtube_video_count_comment_column_name, "integer", (col) =>
+      col.defaultTo(null),
+    )
     .execute();
 }
 
