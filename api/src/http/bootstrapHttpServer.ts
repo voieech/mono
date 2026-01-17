5d0beb4e4939709744146a8837e21afffa4c9da1
Jaime <jaimeloeuf@gmail.com>
2026-01-17 23:51:42 +0800

    Refactor out featured content API routes.

--- api/src/http/bootstrapHttpServer.ts
+++ api/src/http/bootstrapHttpServer.ts
@@ -3,12 +3,12 @@ import type { Episode, Channel } from "dto";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express from "express";
-import { sql } from "kysely";
 
 import { apiDB, genPodcastEpisodeBaseQuery } from "../kysely/index.js";
 import { generateRssXml } from "../rss/index.js";
 import { authRoutes } from "./auth/index.js";
 import { createRoutes } from "./create/index.js";
+import { featuredContentRoutes } from "./featured/index.js";
 import { localeMiddleware } from "./locale/index.js";
 import { appleAppSiteAssociationRoute } from "./others/index.js";
 import { userRoutes } from "./user/index.js";
@@ -33,66 +33,7 @@ export function bootstrapHttpServer() {
     .use(userRoutes)
     .use(createRoutes)
     .use(appleAppSiteAssociationRoute)
-
-    .get("/v1/podcast/featured/channel", async function (req, res) {
-      const rawLimit = Number(req.query["limit"]);
-      const limit = isNaN(rawLimit) || rawLimit === 0 ? 4 : rawLimit;
-
-      if (limit < 1 || limit > 20) {
-        res.status(400).json({
-          error: "Limit must be between 1 and 20",
-        });
-        return;
-      }
-
-      const featuredChannels = await apiDB
-        .selectFrom("podcast_channel")
-        .selectAll()
-        .where((eb) =>
-          eb.or(
-            req.locales.map((locale) => eb("language", "like", `${locale}%`)),
-          ),
-        )
-        // @todo Ordery by popularity
-        .orderBy(sql`random()`)
-        .limit(limit)
-        .execute();
-
-      res.status(200).json(featuredChannels satisfies Array<Channel>);
-    })
-
-    .get("/v1/podcast/featured/episodes", async function (req, res) {
-      const rawLimit = Number(req.query["limit"]);
-      const limit = isNaN(rawLimit) || rawLimit === 0 ? 4 : rawLimit;
-
-      if (limit < 1 || limit > 20) {
-        res.status(400).json({
-          error: "Limit must be between 1 and 20",
-        });
-        return;
-      }
-
-      const featuredEpisodes = await genPodcastEpisodeBaseQuery()
-        .groupBy([
-          "podcast_episode.id",
-          "audio.public_url",
-          "audio.length",
-          "podcast_channel.name",
-        ])
-        .where((eb) =>
-          eb.or(
-            req.locales.map((locale) =>
-              eb("podcast_episode.language", "like", `${locale}%`),
-            ),
-          ),
-        )
-        // @todo Ordery by popularity
-        .orderBy("podcast_episode.created_at", "desc")
-        .limit(limit)
-        .execute();
-
-      res.status(200).json(featuredEpisodes satisfies Array<Episode>);
-    })
+    .use(featuredContentRoutes)
 
     /**
      * Get a list of reccomendations to play next after a given podcast
---
+++ api/src/http/featured/featuredContentRoutes.ts
@@ -0,0 +1,69 @@
+import type { Episode, Channel } from "dto";
+
+import express from "express";
+import { sql } from "kysely";
+
+import { apiDB, genPodcastEpisodeBaseQuery } from "../../kysely/index.js";
+
+export const featuredContentRoutes = express
+  .Router()
+
+  .get("/v1/podcast/featured/channel", async function (req, res) {
+    const rawLimit = Number(req.query["limit"]);
+    const limit = isNaN(rawLimit) || rawLimit === 0 ? 4 : rawLimit;
+
+    if (limit < 1 || limit > 20) {
+      res.status(400).json({
+        error: "Limit must be between 1 and 20",
+      });
+      return;
+    }
+
+    const featuredChannels = await apiDB
+      .selectFrom("podcast_channel")
+      .selectAll()
+      .where((eb) =>
+        eb.or(
+          req.locales.map((locale) => eb("language", "like", `${locale}%`)),
+        ),
+      )
+      // @todo Ordery by popularity
+      .orderBy(sql`random()`)
+      .limit(limit)
+      .execute();
+
+    res.status(200).json(featuredChannels satisfies Array<Channel>);
+  })
+
+  .get("/v1/podcast/featured/episodes", async function (req, res) {
+    const rawLimit = Number(req.query["limit"]);
+    const limit = isNaN(rawLimit) || rawLimit === 0 ? 4 : rawLimit;
+
+    if (limit < 1 || limit > 20) {
+      res.status(400).json({
+        error: "Limit must be between 1 and 20",
+      });
+      return;
+    }
+
+    const featuredEpisodes = await genPodcastEpisodeBaseQuery()
+      .groupBy([
+        "podcast_episode.id",
+        "audio.public_url",
+        "audio.length",
+        "podcast_channel.name",
+      ])
+      .where((eb) =>
+        eb.or(
+          req.locales.map((locale) =>
+            eb("podcast_episode.language", "like", `${locale}%`),
+          ),
+        ),
+      )
+      // @todo Ordery by popularity
+      .orderBy("podcast_episode.created_at", "desc")
+      .limit(limit)
+      .execute();
+
+    res.status(200).json(featuredEpisodes satisfies Array<Episode>);
+  });
