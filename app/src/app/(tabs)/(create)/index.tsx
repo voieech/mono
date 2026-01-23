a54ff4f45c28711a8dc9c16897f490215169c0ba
Jaime <jaimeloeuf@gmail.com>
2026-01-23 21:02:36 +0800

    Create WIP create content home page.

---
+++ app/src/app/(tabs)/(create)/index.tsx
@@ -0,0 +1,147 @@
+import { Trans } from "@lingui/react/macro";
+import { Image } from "expo-image";
+import { Link } from "expo-router";
+import * as WebBrowser from "expo-web-browser";
+import { View, Platform } from "react-native";
+
+import { SafeScrollViewContainer, ThemedView, ThemedText } from "@/components";
+import { Colors } from "@/constants";
+
+export default function CreateTabHomeScreen() {
+  const contentSourceFeedbackLink = "https://forms.gle/wTFB2S6rhPxhG27n7";
+  return (
+    <>
+      <SafeScrollViewContainer>
+        <ThemedView
+          style={{
+            flexDirection: "column",
+            rowGap: 4,
+            paddingBottom: 40,
+          }}
+        >
+          <ThemedText type="lg-normal">
+            <Trans>Create your own content</Trans>
+          </ThemedText>
+          <ThemedText type="base-light">
+            <Trans>
+              Pick a content source to start creating content in your own style!
+            </Trans>
+          </ThemedText>
+        </ThemedView>
+        <ThemedView
+          style={{
+            flexDirection: "column",
+            rowGap: 24,
+          }}
+        >
+          <Link
+            href={{
+              pathname: "/(tabs)/(create)/youtube-video-summary-create",
+            }}
+          >
+            <ThemedView
+              style={{
+                flex: 1,
+                flexDirection: "row",
+                borderRadius: 16,
+              }}
+            >
+              <Image
+                source={require("@/assets/images/content-sources/youtube.png")}
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  maxWidth: 128,
+                  borderTopLeftRadius: 16,
+                  borderBottomLeftRadius: 16,
+                  backgroundColor: Colors.white,
+                }}
+                contentFit="cover"
+              />
+              <ThemedView
+                style={{
+                  flex: 1,
+                  borderTopRightRadius: 16,
+                  borderBottomRightRadius: 16,
+                  paddingVertical: 24,
+                  paddingHorizontal: 16,
+                  rowGap: 8,
+                  backgroundColor: Colors.neutral800,
+                }}
+              >
+                <ThemedText type="lg-normal">
+                  <Trans>YouTube</Trans>
+                </ThemedText>
+                <ThemedText numberOfLines={3}>
+                  <Trans>
+                    Summarise a YouTube video into a podcast episode.
+                  </Trans>
+                </ThemedText>
+              </ThemedView>
+            </ThemedView>
+          </Link>
+          <Link
+            href={contentSourceFeedbackLink}
+            onPress={async (e: any) => {
+              // Allow default behaviour to continue for web platform
+              if (Platform.OS === "web") {
+                return;
+              }
+
+              // Prevent default behavior of opening system browser, and open in
+              // in-app modal browser instead
+              e.preventDefault();
+              await WebBrowser.openBrowserAsync(contentSourceFeedbackLink);
+            }}
+          >
+            <ThemedView
+              style={{
+                flex: 1,
+                flexDirection: "row",
+                borderRadius: 16,
+              }}
+            >
+              <Image
+                source={require("@/assets/images/content-sources/coming-soon.jpg")}
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  maxWidth: 128,
+                  borderTopLeftRadius: 16,
+                  borderBottomLeftRadius: 16,
+                  backgroundColor: Colors.white,
+                }}
+                contentFit="cover"
+              />
+              <ThemedView
+                style={{
+                  flex: 1,
+                  borderTopRightRadius: 16,
+                  borderBottomRightRadius: 16,
+                  paddingVertical: 24,
+                  paddingHorizontal: 16,
+                  rowGap: 8,
+                  backgroundColor: Colors.neutral800,
+                }}
+              >
+                <ThemedText type="lg-normal">
+                  <Trans>Want more?</Trans>
+                </ThemedText>
+                <ThemedText numberOfLines={3}>
+                  <Trans>
+                    Tell us what other content sources you would like us to add!
+                  </Trans>
+                </ThemedText>
+              </ThemedView>
+            </ThemedView>
+          </Link>
+        </ThemedView>
+        <View
+          style={{
+            paddingBottom: 64,
+          }}
+        />
+      </SafeScrollViewContainer>
+    </>
+  );
+}
