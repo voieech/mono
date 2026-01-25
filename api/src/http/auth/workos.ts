bd5cd6afbd7e40c426a6e29fa2c0b60b87dfdf29
Jaime <jaimeloeuf@gmail.com>
2026-01-25 19:58:09 +0800

    Add WORKOS_ACTIONS_SECRET validated env var to API.

--- api/src/http/auth/workos.ts
+++ api/src/http/auth/workos.ts
@@ -4,6 +4,10 @@ if (process.env["WORKOS_API_KEY"] === undefined) {
   throw new Error(`process.env.WORKOS_API_KEY is undefined`);
 }
 
+if (process.env["WORKOS_ACTIONS_SECRET"] === undefined) {
+  throw new Error(`process.env.WORKOS_ACTIONS_SECRET is undefined`);
+}
+
 if (process.env["WORKOS_CLIENT_ID"] === undefined) {
   throw new Error(`process.env.WORKOS_CLIENT_ID is undefined`);
 }
@@ -17,6 +21,7 @@ if (process.env["WORKOS_REDIRECT_URI"] === undefined) {
 }
 
 export const WORKOS_API_KEY = process.env["WORKOS_API_KEY"];
+export const WORKOS_ACTIONS_SECRET = process.env["WORKOS_ACTIONS_SECRET"];
 export const WORKOS_CLIENT_ID = process.env["WORKOS_CLIENT_ID"];
 export const WORKOS_COOKIE_PASSWORD = process.env["WORKOS_COOKIE_PASSWORD"];
 export const WORKOS_REDIRECT_URI = process.env["WORKOS_REDIRECT_URI"];
