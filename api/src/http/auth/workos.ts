95f9370fd63c5c35433e7c455a3dfc8b0966a9d3
Jaime <jaimeloeuf@gmail.com>
2026-01-26 00:02:25 +0800

    Add more WorkOS validated env vars.

--- api/src/http/auth/workos.ts
+++ api/src/http/auth/workos.ts
@@ -4,6 +4,14 @@ if (process.env["WORKOS_API_KEY"] === undefined) {
   throw new Error(`process.env.WORKOS_API_KEY is undefined`);
 }
 
+if (process.env["WORKOS_WEBHOOK_SECRET"] === undefined) {
+  throw new Error(`process.env.WORKOS_WEBHOOK_SECRET is undefined`);
+}
+
+if (process.env["WORKOS_WEBHOOK_PATH_SECRET"] === undefined) {
+  throw new Error(`process.env.WORKOS_WEBHOOK_PATH_SECRET is undefined`);
+}
+
 if (process.env["WORKOS_ACTIONS_SECRET"] === undefined) {
   throw new Error(`process.env.WORKOS_ACTIONS_SECRET is undefined`);
 }
@@ -21,6 +29,9 @@ if (process.env["WORKOS_REDIRECT_URI"] === undefined) {
 }
 
 export const WORKOS_API_KEY = process.env["WORKOS_API_KEY"];
+export const WORKOS_WEBHOOK_SECRET = process.env["WORKOS_WEBHOOK_SECRET"];
+export const WORKOS_WEBHOOK_PATH_SECRET =
+  process.env["WORKOS_WEBHOOK_PATH_SECRET"];
 export const WORKOS_ACTIONS_SECRET = process.env["WORKOS_ACTIONS_SECRET"];
 export const WORKOS_CLIENT_ID = process.env["WORKOS_CLIENT_ID"];
 export const WORKOS_COOKIE_PASSWORD = process.env["WORKOS_COOKIE_PASSWORD"];
