25f521d0fd383ea2dd86f8ec5e6bd0fdfe46aeea
Jaime <jaimeloeuf@gmail.com>
2026-02-21 20:59:22 +0800

    Add fallback error logger:     Instead of just not logging, use the global logger as a fallback in     errorHandler middleware if request specific logger is not available.

--- api/src/http/errorHandler.ts
+++ api/src/http/errorHandler.ts
@@ -3,6 +3,7 @@ import type { Request, Response, NextFunction } from "express";
 import { convertUnknownCatchToError } from "convert-unknown-catch-to-error";
 
 import { HttpTransformerableException } from "../exceptions/index.js";
+import { logger } from "../logger/logger.js";
 import { HttpStatus } from "./HttpStatus.js";
 import { JSendError } from "./JSend.js";
 
@@ -23,19 +24,25 @@ export function errorHandler(
 ) {
   const error = convertUnknownCatchToError(e);
 
-  // Adding optional chaining to this entire call chain, since there is a chance
-  // that req.logger is not attached before errorHandler is called. E.g. when
-  // an error is thrown in a middleware that runs before loggerMiddleware or if
-  // the loggerMiddleware itself threw an error.
-  req?.logger
-    ?.withError(error)
-    ?.withMetadata({
+  // There is a chance that req.logger is not attached before errorHandler is
+  // called (even though for the sake of simpler global Express.Request type
+  // definition it is defined as always non-null). E.g. when an error is thrown
+  // in a middleware that runs before loggerMiddleware or if the
+  // loggerMiddleware itself threw an error. So instead of not logging, we will
+  // fall back to the global logger and use that instead.
+  const errorLogger = req.logger ?? logger;
+
+  errorLogger
+    .withError(error)
+    .withMetadata({
       // This is attached for debugging when there is an error because req.body
       // is not logged by default to reduce log size and to prevent leaking PII
       // during normal operations.
-      requestBodyForDebuggingOnError: req.body,
+      // If `req.body` is undefined, convert it to null instead of leaving as
+      // undefined since undefined will be scrubbed from the object when logged.
+      requestBodyForDebuggingOnError: req.body ?? null,
     })
-    ?.error(error.message);
+    .error(error.message);
 
   if (error instanceof HttpTransformerableException) {
     const { httpStatusCode, jsendData } = error.transformToHttpResponseData();
