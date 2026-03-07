2f68f18663c448f7c448f1d0ffe7f04bbfe01197
Jaime <jaimeloeuf@gmail.com>
2026-03-08 00:09:27 +0800

    Add zod schema for qstash event objects:     Since arbitrary data can be sent to qstash, adding zod schema to     fix the event shape.

---
+++ api/src/controllers-http/qstashWebhooks/qstashEventSchema.ts
@@ -0,0 +1,42 @@
+import { z } from "zod";
+
+const qstashBaseEventSchema = z.object({
+  /**
+   * Opaque unique ID using their custom format
+   */
+  qstashMessageID: z.string(),
+
+  /**
+   * UUID set by the publisher (our own code)
+   */
+  id: z.uuidv4(),
+
+  /**
+   * When was this "event" created at?
+   */
+  createdAt: z.iso.datetime(),
+
+  /**
+   * A fixed set of allowed event type names, each with their own different
+   * corresponding properties. The specific event zod schemas will individually
+   * specify this as literal strings.
+   */
+  type: z.string(),
+});
+
+/**
+ * Union of all QStash event object types
+ *
+ * Extend this whenever you need to handle a new incoming event type from qstash
+ */
+export const qstashEventSchema = z.discriminatedUnion("type", [
+  qstashBaseEventSchema.safeExtend({
+    type: z.literal("podcast-episode-created"),
+    data: z.object({
+      podcastEpisodeID: z.uuidv4(),
+      podcastChannelID: z.uuidv4(),
+    }),
+  }),
+]);
+
+export type QstashEvent = z.infer<typeof qstashEventSchema>;
