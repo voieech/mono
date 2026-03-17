70524bf18ae31b123dd4e722bcb0696df350e4ba
Jaime <jaimeloeuf@gmail.com>
2026-03-17 20:24:29 +0800

    Update custom NotFoundError to better extend super.

--- app/src/errors/NotFoundError.ts
+++ app/src/errors/NotFoundError.ts
@@ -1,1 +1,8 @@
-export class NotFoundError extends Error {}
+export class NotFoundError extends Error {
+  constructor(message?: string) {
+    super(message);
+    this.name = "NotFoundError";
+    // Restore prototype chain for `instanceof` to work
+    Object.setPrototypeOf(this, NotFoundError.prototype);
+  }
+}
