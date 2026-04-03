401a42ae8bdcb0ad094a0b37b9d2c2c4b229a5f6
Jaime <jaimeloeuf@gmail.com>
2026-04-04 00:39:04 +0800

    Add initial route to temporarily fix navigation bug.

--- app/src/app/(tabs)/profile/_layout.tsx
+++ app/src/app/(tabs)/profile/_layout.tsx
@@ -1,6 +1,10 @@
 import { useLingui } from "@lingui/react/macro";
 import { Stack } from "expo-router";
 
+export const unstable_settings = {
+  initialRouteName: "index",
+};
+
 export default function ProfileLayout() {
   const { t } = useLingui();
   return (
